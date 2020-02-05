32eaf4fe097d3d647645c27a423182dabfd7393a
Blake <60404085+betill16@users.noreply.github.com>
2020-02-05 14:33:20 -0500

    Create BlakesHockeyStory.py

---
+++ BlakesHockeyStory.py
@@ -0,0 +1,14 @@
+print("    THE  FINAL  COUNTDOWN    ")
+print("We were in the locker room,It was 6:45 and the game of our lives at home (PPG Paints Area) aginst the capitals a division rival, it decided if we would advance to the playoffs.")
+print("We were sitting there in the freezing locker room waiting for our names to come running through the announcers mic. ")
+print("Then with the cheer of the crowd we heard names being called, and soon they called my name, Please Welcome BLAKE TILLMAN)
+print("I was the most nervous I had ever been for a game even for being a rookie that year")
+print("I skated out on the ice and the game had begun")
+option1 == input(" Get the puck ,  Give the puck off")
+
+if option1 == "Get the puck":
+    print("We had won the puck drop for the first period, my teammate Johnny Woo passed to me and I hit a slapshot for the first goal")
+    print("We were up 1-0 withing the first 2 minutes with 18 to go and things were looking good")
+
+if option1 == "Give the puck off":
+    print("They won the puck drop and chewed clock keeping mostly on are side of the ice for about 4 minutes and then scored, they were up 1-0")
