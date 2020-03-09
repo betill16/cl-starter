38228326a7ebc90e70152495e6653ea8894b878b
Blake <60404085+betill16@users.noreply.github.com>
2020-03-09 13:58:53 -0400

    Update BlakesHockeyStory.py

--- BlakesHockeyStory.py
+++ BlakesHockeyStory.py
@@ -11,6 +11,7 @@ def intro():
         print("We had won the puck drop for the first period, my teammate Johnny Woo passed to me and I hit a slapshot for the first goal")
         print("We were up 1-0 within the first 2 minutes with 18 to go and things were off to a good start")
         drop()
+         
     if option1 == "Give the puck off":
         print("They won the puck drop and chewed clock keeping the puck mostly on are side of the ice for about 4 minutes and then scored, they were up 1-0")
         scoredon()
@@ -23,10 +24,12 @@ def drop():
     if option2 == "Go for the goal":
         print("shot the puck and missed and they took it for a breakaway they missed the first shot but played good defense and ended up scoring a couple minutes later , we were tied 1-1.")
         print("After that the game went back and forth from one side of the ice to the other and no one scored again that period")
+        print("We came out the second period and that was also scoreless, things were really bland and then the last 10 minutes came")
 
     if option2 == "pass it off":
         print("I passed it to other teammate Szander, are center. He took the shot and made it we were up 2-0 ")
         print("After that the game went back and forth from one side of the ice to the other and no one scored again that period")
+        print("We came out the second period and that was also scoreless, things were really bland and then the last 10 minutes came")
 
               
 
@@ -34,14 +37,53 @@ def drop():
 def scoredon():
     print("There was one thing that went are way though, My teammate johnny won the puck drop this time")
     print("He passed to me and with a swift though I")
+    option2 = input("Go for the goal, pass it off")
     if option2 == "Go for the goal":
         print("shot the puck and missed and they took it for a breakaway, they missed the first shot but played good defense and ended up scoring a couple minutes later, we were down 2-0.")
         print("After that the game went back and forth from one side of the ice to the other and no one scored again that period")
+        print("We came out the second period and that was also scoreless, things were really bland and then the last 10 minutes came")
 
 
     if option2 == "pass it off":
         print("I passed it to other teammate Szander, are center. He took the shot and made it we were tied 1-1")
         print("After that the game went back and forth from one side of the ice to the other and no one scored again that period")
+        print("We came out the second period and that was scoreless aswell, things were really bland and then the last 10 minutes came")
+
+
+
+def win():
+    option3 == input"Attacking, Defending":
+    if option3 == "Attacking":
+        print("We had just gotten it over to their side of the ice, Szander passed to Johnny, Johnny passed to me, and I shot and missed.")
+        print("It bounced back to the opposing team and we stuck defending")
+        print("But we managed to fight them off and johnny took it down the ice for a slapshot score")
+        print("We had won with a flawless victory 3-0")
+    if option3 == "Defending":
+        print("We were defending, what felt like the entire 3rd period, going back and forth the whole time.")
+        print("For nothing because they scored, it was 2-1 with 5 minutes left on the game clock.")
+        print("They got close to scoring a few times but we came out victorious.")
+        print("winning 2-1.")
+
+def OTLoss():
+    option3 == input"Attacking, Defending":
+    if option3 == "Attacking":
+        print("I was coming down the with the puck by my side, I passed to Szander and he scored")
+        print("We took the lead again 2-1")
+        print("But the game wasn't over, we had scored that a little early")
+        print("Giving them time for another goal tying in 2-2 and before you knew it we went to OverTime")
+        print("It was intense lots of passes, shots, and a little rough and sadly they got the edge and scored")
+        print("We lost 2-3")
+    if option3 
+
+def OTWin():
+    option3 == input"Attacking, Defending":
+    if option3 == "Attacking":
+
+def Loss():
+    option3 == input"Attacking, Defending":
+    if option3 == "Attacking":
+
+
 
         
 
